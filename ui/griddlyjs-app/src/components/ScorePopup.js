9ccc0767698b48fd13f3f6a8068273e178c2eec5
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-12-12 13:29:56 +0000

    makes score popup more obvious

--- ui/griddlyjs-app/src/components/ScorePopup.js
+++ ui/griddlyjs-app/src/components/ScorePopup.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { Modal } from "./Modal";
+import { motion, AnimatePresence } from "framer-motion";
 
 const ScorePopup = ({ score }) => {
   const [open, setOpen] = useState(false);
@@ -16,14 +16,25 @@ const ScorePopup = ({ score }) => {
   useEffect(() => {
     if (scoreDelta > 0) {
       setOpen(true);
-      setTimeout(() => setOpen(false), 1000);
+      setTimeout(() => setOpen(false), 2000);
     }
   }, [scoreDelta]);
 
   return (
-    <Modal open={open} className="score-popup">
-      +{scoreDelta} points!
-    </Modal>
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          key="score-popup"
+          className="score-popup"
+          initial={{ opacity: 0, top: 10 }}
+          animate={{ opacity: 1, top: -60 }}
+          exit={{ opacity: 0, top: 0 }}
+          transition={{ duration: 0.4 }}
+        >
+          +{scoreDelta} points!
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
