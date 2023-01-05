32a197c6b4a6b0334ca62ef18d91896bfbdb5c60
Max Taylor-Davies <maxtaylordavies@gmail.com>
2023-01-05 17:51:58 +0000

    adds code for similarity barplots

--- scripts/analysis/analysis.py
+++ scripts/analysis/analysis.py
@@ -1,14 +1,42 @@
 import collections
-import csv
 import json
 from os import path, listdir
-import numpy as np
-import matplotlib.pyplot as plt
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_context("paper")
+sns.set_theme()
+
+AGENT_TRAJECTORIES = [
+    {
+        0: "1,1,1,1,1,1,1,1,1,1,1,1",
+        1: "3,3,3,3,3,3,3,3,3,3,3,3",
+        2: "2,2,2,2,2,2,2,2,2,2,2,2",
+        3: "4,4,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2",
+        4: "4,4,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2",
+        5: "1,1,1,1,1,1,1,1,1,1,1,1",
+        6: "3,3,3,3,3,3,3,3,3,3,3,3",
+    },
+    {
+        0: "2,2,2,2,2,2,2,2,2,2,2,2",
+        1: "1,1,1,1,1,1,1,1,1,1,1,1",
+        2: "3,3,3,3,3,3,3,3,3,3,3,3",
+        3: "4,4,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2",
+        4: "4,4,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2",
+        5: "3,3,3,3,3,3,3,3,3,3,3,3",
+        6: "1,1,1,1,1,1,1,1,1,1,1,1",
+    },
+]
 MOVE_MAP = {"1": [-1, 0], "2": [0, 1], "3": [1, 0], "4": [0, -1]}
-DATA_DIR = "data"
+DATA_DIR = "../../data"
 SESSION_DIR = path.join(DATA_DIR, "sessions")
 TRAJECTORY_DIR = path.join(DATA_DIR, "trajectories")
+
+
+def logValue(msg, val):
+    print(f"{msg}: {val}")
 
 
 def loadJSON(fp):
@@ -51,14 +79,14 @@ def computeDominantDirection(path):
     return max(counts, key=counts.get)
 
 
-def similarity1(path1, path2):
+def simCont(path1, path2):
     """
     returns 1 / (d + 1), where d is the mean distance between the two paths
     """
     return 1 / (distanceBetweenPaths(path1, path2, norm=False) + 1)
 
 
-def similarity2(path1, path2):
+def simBin(path1, path2):
     """
     returns 1 if path1 and path2 have the same dominant direction, otherwise 0
     """
@@ -79,84 +107,87 @@ def similarity2(path1, path2):
 #     return 1 / (distanceBetweenPaths(path1, path2, type="max") + 1)
 
 
-def analyseData(levels):
-    results = []
+def loadData(experimentIDs=[], simType="cont"):
+    """
+    creates a DataFrame of similarity data from session json files
+    """
+    experimentFilter = (
+        lambda s: s["experimentId"] in experimentIDs if experimentIDs else True
+    )
+    levels = list(range(7))
+    simFunc = simCont if simType == "cont" else simBin
 
-    # load agent trajectories
-    agentTrajectories = [
-        loadJSON("data/trajectories/multijewel_a-0001.json")["paths"],
-        loadJSON("data/trajectories/multijewel_a-0002.json")["paths"],
-    ]
-
-    # generate results
+    # load sessions
     filepaths = [fp for fp in listdir(SESSION_DIR) if fp.endswith(".json")]
-    for fp in filepaths:
-        # load session data and record ids
-        sess, row = loadJSON(path.join(SESSION_DIR, fp)), {}
-        row["session_id"], row["human_id"] = sess["id"], sess["humanId"]
+    sessions = [loadJSON(path.join(SESSION_DIR, fp)) for fp in filepaths]
+    sessions = [s for s in sessions if experimentFilter(s)]
 
-        # try to load human trajectory. if it's not stored, skip to next session
-        try:
-            humanTrajectory = loadJSON(
-                path.join(TRAJECTORY_DIR, f"multijewel_{sess['humanId']}.json")
-            )["paths"]
-        except:
-            continue
+    # initialise data dict
+    cols = ["id", "group"] + [
+        f"level_{l + 1}_agent_{a + 1}" for l in levels for a in [0, 1]
+    ]
+    dataDict = {col: [] for col in cols}
 
-        # record which agent the player shared their goal with
-        row["correct_model"] = (
-            sess["utility"]["goals"].index(max(sess["utility"]["goals"])) + 1
-        )  # 1 if goal matches agent 1, 2 if it matches agent 2
-
-        # record both continuous and binary similarity measures between the player's trajectory and both agents
+    # populate data dict
+    for s in sessions:
+        dataDict["id"].append(s["id"])
+        dataDict["group"].append(f"group {int(s['utility']['goals'][1] == 50) + 1}")
         for l in levels:
-            for i in [0, 1]:
-                row[f"continuous_sim_level_{l}_agent_{i+1}"] = similarity1(
-                    humanTrajectory[l], agentTrajectories[i][l]
-                )
-                row[f"binary_sim_level_{l}_agent_{i+1}"] = similarity2(
-                    humanTrajectory[l], agentTrajectories[i][l]
+            for a in [0, 1]:
+                dataDict[f"level_{l + 1}_agent_{a + 1}"].append(
+                    simFunc(AGENT_TRAJECTORIES[a][l], s["trajectories"][str(l)])
                 )
 
-        results.append(row)
-
-    return results
-
-
-def saveResults(results):
-    keys = results[0].keys()
-    with open("tmp/results.csv", "w") as f:
-        w = csv.DictWriter(f, keys)
-        w.writeheader()
-        w.writerows(results)
+    # convert to dataframe and return
+    return pd.DataFrame(data=dataDict)
 
 
-def averageVar(results, key):
-    vals = [row[key] for row in results]
-    return sum(vals) / len(vals)
+def makePlotForLevels(data, levels, outFolder=".", outFormat="pdf"):
+    fig, axs = plt.subplots(
+        ncols=len(levels), sharey=True, figsize=(20, 4)
+    )
+    for i, level in enumerate(levels):
+        melted = data.rename(
+            columns={
+                f"level_{level}_agent_1": "agent 1",
+                f"level_{level}_agent_2": "agent 2",
+            }
+        ).melt(
+            id_vars=["group"],
+            value_vars=["agent 1", "agent 2"],
+            var_name="",
+            value_name="similarity",
+        )
+        g = sns.barplot(
+            data=melted,
+            x="group",
+            y="similarity",
+            hue="",
+            errorbar="se",
+            errcolor="grey",
+            errwidth=1.4,
+            capsize=0.04,
+            palette=sns.color_palette("husl", 10),
+            ax=axs[i],
+        )
+        g.set(
+            xlabel="",
+            ylim=[0, 1],
+            ylabel=("mean trajectory similarity" if i == 0 else ""),
+            title=f"Level {level}",
+        )
+        if i < len(levels) - 1:
+            g.legend_.remove()
+
+    fig.tight_layout()
+    plt.savefig(
+        path.join(outFolder, f"barplot_levels_{levels[0]}-{levels[-1]}.{outFormat}")
+    )
 
 
 def main():
-    # levels = [str(i) for i in range(3, 7)]
-    levels = ["3", "4"]
-    results = analyseData(levels)
-
-    saveResults(results)
-
-    heatmap = np.zeros((2, 2 * len(levels)))
-    for model in [1, 2]:
-        rows = [r for r in results if r["correct_model"] == model]
-        for i, l in enumerate(levels):
-            heatmap[model - 1, (i * 2)] = averageVar(
-                rows, f"continuous_sim_level_{l}_agent_1"
-            )
-            heatmap[model - 1, (i * 2) + 1] = averageVar(
-                rows, f"continuous_sim_level_{l}_agent_2"
-            )
-
-    plt.imshow(heatmap)
-    plt.colorbar()
-    plt.show()
+    data = loadData()
+    makePlotForLevels(data, [1, 2, 3, 4, 5, 6, 7], outFormat="png")
 
 
 if __name__ == "__main__":
