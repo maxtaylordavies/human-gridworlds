2dc598bae52a7c4526bba5ab460a63a42333a8b2
Max Taylor-Davies <maxtaylordavies@gmail.com>
2023-01-09 13:19:57 +0000

    adds code for trajectory visualisation

--- scripts/analysis/analysis.py
+++ scripts/analysis/analysis.py
@@ -3,11 +3,10 @@ import json
 from os import path, listdir
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-sns.set_context("paper")
-sns.set_theme()
+import seaborn as sns
 
 AGENT_TRAJECTORIES = [
     {
@@ -29,7 +28,17 @@ AGENT_TRAJECTORIES = [
         6: "1,1,1,1,1,1,1,1,1,1,1,1",
     },
 ]
-MOVE_MAP = {"1": [-1, 0], "2": [0, 1], "3": [1, 0], "4": [0, -1]}
+GRID_SIZES = [
+    (14, 25),
+    (14, 25),
+    (14, 25),
+    (15, 19),
+    (15, 19),
+    (14, 25),
+    (14, 25),
+]
+START_POSITIONS = [(12, 12), (12, 12), (12, 12), (5, 9), (5, 9), (6, 12), (6, 12)]
+MOVE_MAP = {"1": [0, -1], "2": [-1, 0], "3": [0, 1], "4": [1, 0]}
 DATA_DIR = "../../data"
 SESSION_DIR = path.join(DATA_DIR, "sessions")
 TRAJECTORY_DIR = path.join(DATA_DIR, "trajectories")
@@ -45,11 +54,11 @@ def loadJSON(fp):
     return data
 
 
-def convertPathToCoords(path):
-    coords = [[0, 0]]
+def convertPathToCoords(path, startPos=(0, 0)):
+    coords = [startPos]
     for move in path.split(","):
         coords.append(
-            [coords[-1][0] + MOVE_MAP[move][0], coords[-1][1] + MOVE_MAP[move][1]]
+            (coords[-1][0] + MOVE_MAP[move][0], coords[-1][1] + MOVE_MAP[move][1])
         )
     return coords
 
@@ -93,34 +102,23 @@ def simBin(path1, path2):
     return int(computeDominantDirection(path1) == computeDominantDirection(path2))
 
 
-# def similarity3(path1, path2):
-#     """
-#     returns 1 - (d / l), where d is the mean distance between the two paths and l is the (longer) path length
-#     """
-#     return 1 - distanceBetweenPaths(path1, path2)
-
-
-# def similarity4(path1, path2):
-#     """
-#     returns 1 / (d + 1), where d is the maximum distance between the two paths
-#     """
-#     return 1 / (distanceBetweenPaths(path1, path2, type="max") + 1)
-
-
-def loadData(experimentIDs=[], simType="cont"):
-    """
-    creates a DataFrame of similarity data from session json files
-    """
+def loadSessions(experimentIDs=[]):
     experimentFilter = (
         lambda s: s["experimentId"] in experimentIDs if experimentIDs else True
     )
-    levels = list(range(7))
-    simFunc = simCont if simType == "cont" else simBin
 
-    # load sessions
+    # load sessions and return
     filepaths = [fp for fp in listdir(SESSION_DIR) if fp.endswith(".json")]
     sessions = [loadJSON(path.join(SESSION_DIR, fp)) for fp in filepaths]
-    sessions = [s for s in sessions if experimentFilter(s)]
+    return [s for s in sessions if experimentFilter(s)]
+
+
+def computeSimilarityData(sessions, simType="cont"):
+    """
+    creates a DataFrame of similarity data from list of session objects
+    """
+    levels = list(range(7))
+    simFunc = simCont if simType == "cont" else simBin
 
     # initialise data dict
     cols = ["id", "group"] + [
@@ -142,10 +140,11 @@ def loadData(experimentIDs=[], simType="cont"):
     return pd.DataFrame(data=dataDict)
 
 
-def makePlotForLevels(data, levels, outFolder=".", outFormat="pdf"):
-    fig, axs = plt.subplots(
-        ncols=len(levels), sharey=True, figsize=(20, 4)
-    )
+def plotSimilarityForLevels(data, levels, outFolder=".", outFormat="pdf"):
+    sns.set_context("paper")
+    sns.set_theme()
+
+    fig, axs = plt.subplots(ncols=len(levels), sharey=True, figsize=(20, 4))
     for i, level in enumerate(levels):
         melted = data.rename(
             columns={
@@ -185,9 +184,73 @@ def makePlotForLevels(data, levels, outFolder=".", outFormat="pdf"):
     )
 
 
+def plotTrajectories(trajectories, gridSize, startPos):
+    nRows, nCols = gridSize
+    grid = np.zeros((nRows, nCols, 3))
+
+    for traj in trajectories:
+        coords = convertPathToCoords(traj, startPos)
+        grid[coords[0]] = np.array([0, 1, 0])
+        for (r, c) in coords[1:]:
+            if r < nRows and c < nCols:
+                grid[r, c] += (1 / len(trajectories)) * np.ones((3))
+
+    return grid
+
+
+def visualiseTrajectoriesForLevels(levels, sessions, similarityData):
+    fig, axs = plt.subplots(ncols=len(levels))
+
+    for i, level in enumerate(levels):
+        grids, gridSize, startPos = (
+            [],
+            GRID_SIZES[level - 1],
+            START_POSITIONS[level - 1],
+        )
+
+        # visualise trajectory of demonstrator agents
+        grids += [
+            plotTrajectories([agent[level - 1]], gridSize, startPos)
+            for agent in AGENT_TRAJECTORIES
+        ]
+
+        # visualise trajectory of participants
+        group1Trajectories, group2Trajectories = [], []
+        for j, s in enumerate(sessions):
+            traj = s["trajectories"][str(level - 1)]
+            if similarityData.loc[j]["group"] == "group 1":
+                group1Trajectories.append(traj)
+            else:
+                group2Trajectories.append(traj)
+
+        grids += [
+            plotTrajectories(group1Trajectories, gridSize, startPos),
+            plotTrajectories(group2Trajectories, gridSize, startPos),
+        ]
+
+        nRows, nCols = gridSize
+        margin = 2
+        combinedGrid = np.ones(((nRows * 2) + margin, (nCols * 2) + margin, 3))
+
+        combinedGrid[0:nRows, 0:nCols] = grids[0]
+        combinedGrid[0:nRows, nCols + margin :] = grids[1]
+        combinedGrid[nRows + margin :, :nCols] = grids[2]
+        combinedGrid[nRows + margin :, nCols + margin :] = grids[3]
+
+        axs[i].imshow(combinedGrid, cmap="gray")
+        axs[i].set_title(f"Level {level}")
+        axs[i].set_axis_off()
+
+    fig.set_tight_layout(True)
+    plt.show()
+
+
 def main():
-    data = loadData()
-    makePlotForLevels(data, [1, 2, 3, 4, 5, 6, 7], outFormat="png")
+    sessions = loadSessions()
+    similarityData = computeSimilarityData(sessions)
+
+    # plotSimilarityForLevels(data, [1, 2, 3, 4, 5, 6, 7], outFormat="png")
+    visualiseTrajectoriesForLevels([1, 2, 3, 4, 5, 6, 7], sessions, similarityData)
 
 
 if __name__ == "__main__":
