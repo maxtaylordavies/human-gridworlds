410c0e45bcf15c3239fabc3191fda076063ac10d
Max Taylor-Davies <maxtaylordavies@gmail.com>
2023-06-13 13:07:43 +0100

    removes console log

--- ui/griddlyjs-app/src/App.js
+++ ui/griddlyjs-app/src/App.js
@@ -47,11 +47,6 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    console.log("expState: ", expState);
-    console.log("uiState: ", uiState);
-  }, [expState, uiState]);
-
-  useEffect(() => {
     if (expState.session) {
       // fetch data
       fetchData();
