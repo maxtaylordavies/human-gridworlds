27195e5650bd8709f45e28a5b9ddf922e755ab3d
Max Taylor-Davies <maxtaylordavies@gmail.com>
2024-10-31 11:28:46 +0000

    adds score back to info bar

--- ui/griddlyjs-app/src/components/InfoBar.js
+++ ui/griddlyjs-app/src/components/InfoBar.js
@@ -6,6 +6,9 @@ import * as utils from "../utils";
 const InfoBar = () => {
   const expState = useStore((state) => state.expState);
   const playing = useStore((state) => state.gameState.playing);
+  const score = useStore((state) => state.gameState.score);
+
+  const phase = utils.currentPhase(expState);
 
   return (
     <div className="info-bar">
@@ -13,9 +16,11 @@ const InfoBar = () => {
         {playing ? "Playing" : `Observing: ${utils.currentAgentName(expState)}`}
       </div>
       <div className="info-bar-stats">
+        {!phase.objectsHidden &&
+          InfoBarItem("score", score, { fontWeight: "bold" })}
         {InfoBarItem(
           "phase",
-          `${expState.phaseIdx + 1}/${expState.session.phases.length}`
+          `${expState.phaseIdx + 1}/${expState.session.phases.length}`,
         )}
       </div>
     </div>
