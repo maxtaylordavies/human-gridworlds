1673cd07ae71c502cd30e2f41f37d81be5fea575
Max Taylor-Davies <maxtaylordavies@gmail.com>
2024-11-04 15:06:10 +0000

    updates constants.js

--- ui/griddlyjs-app/src/constants.js
+++ ui/griddlyjs-app/src/constants.js
@@ -1,9 +1,10 @@
-export const BASE_ITEM_REWARD = 20;
+export const BASE_ITEM_REWARD = 40;
+export const MOVE_COST = 0.5;
 
-export const INTER_STEP_INTERVAL_MS = 120;
+export const INTER_STEP_INTERVAL_MS = 90;
 export const INTER_LEVEL_INTERVAL_MS = 4000;
-export const INTER_SCENE_INTERVAL_MS = 1000;
-export const LINGER_ON_GOAL_MS = 2000;
+export const INTER_SCENE_INTERVAL_MS = 500;
+export const LINGER_ON_GOAL_MS = 1500;
 export const OBJECT_FEATURE_MAP = {
   goalA: [0, 0],
   goalB: [1, 0],
